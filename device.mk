f3d207aad314270e06b3b70786307caa7845985f
anggialansori404 <anggialansori.rpl4@gmail.com>
2021-10-05 10:04:08 +0700

    Revert "mojito: Include ANXCamera"
    This reverts commit 232c5162c8b80fc14645124f1393e91429538d4d.

--- device.mk
+++ device.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Include ANXCamera
-$(call inherit-product, vendor/xiaomi/ANXCamera/config.mk)
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
